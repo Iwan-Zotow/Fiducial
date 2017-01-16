0d017753f3614630ff7b8d13155fbb87c2230288
Nobody <nobody@nowhere.to.go>
2017-01-16 18:18:47 -0500

    More development

--- import_cup.py
+++ import_cup.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import sys
 import logging
 
+import OCC.TopoDS
 import OCC.Display.SimpleGui
 
 import aocutils.display.topology
@@ -37,28 +38,52 @@ def readSTEP(fname):
 
     return importer.shapes
 
+def str_shape(sh_type):
+    """
+    """
+    types = ["COMPOUND", "COMPSOLID", "SOLID", "SHELL", "FACE", "WIRE", "EDGE", "VERTEX"]
+    return types[sh_type]
+
+def print_shapes():
+    """
+    print default shapes in ascending order, from
+    https://www.opencascade.com/doc/occt-6.9.1/refman/html/class_topo_d_s___shape.html
+    """
+    print("----------------------------")
+    print(OCC.TopAbs.TopAbs_COMPOUND)
+    print(OCC.TopAbs.TopAbs_COMPSOLID)
+    print(OCC.TopAbs.TopAbs_SOLID)
+    print(OCC.TopAbs.TopAbs_SHELL)
+    print(OCC.TopAbs.TopAbs_FACE)
+    print(OCC.TopAbs.TopAbs_WIRE)
+    print(OCC.TopAbs.TopAbs_EDGE)
+    print(OCC.TopAbs.TopAbs_VERTEX)
+    print("----------------------------")
+
 def main(fname):
     """
     Given the file name, read STEP from it and display it
     """
 
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s :: %(levelname)6s :: %(module)20s :: %(lineno)3d :: %(message)s')
-
     shapes = readSTEP(fname)
 
-    print("Nb shapes: %i" % len(shapes))
     print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print("Nb shapes: %i" % len(shapes))
     for shape in shapes:
-        print(shape.ShapeType())  # 2 -> solid
+        print(str_shape(shape.ShapeType()))
     print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
     print(shapes[0])
-    print(dir(shapes[0]))
+    # print(dir(shapes[0]))
     print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
-    display_shapes(shapes)
+    return shapes
 
 if __name__ == "__main__":
 
-    main("cups/XMSGP030A10.01-003 breast_cup_outer_S fiducial wire.STEP") # "XMSGP030A10.01-003 breast_cup_outer_S 214.STEP"
+    logging.basicConfig(level=logging.NOTSET, format='%(asctime)s :: %(levelname)6s :: %(module)20s :: %(lineno)3d :: %(message)s')
+
+    shapes = main("cups/XMSGP030A10.01-003 breast_cup_outer_S fiducial wire.STEP") # "XMSGP030A10.01-003 breast_cup_outer_S 214.STEP"
+
+    # display_shapes(shapes)
 
     sys.exit(0)
